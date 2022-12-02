03f7d80a61dd7212b80fc0226ca1f9a6597a3257
madvorak <dvorakmartinbridge@seznam.cz>
2022-12-02 10:21:04 +0100

    priprava na BrNacht

---
+++ src/sequence_limits_cz.lean
@@ -0,0 +1,42 @@
+-- založeno na Lean Tutorial / 05_sequence_limits.lean
+import resources
+
+/- 
+Užitečná lemmata:
+
+`le_max_left  p q` říká `p ≤ max p q`
+`le_max_right p q` říká `q ≤ max p q`
+
+`eq_of_abs_sub_le_all_pos x y` říká `(∀ ε > 0, |x - y| ≤ ε) → x = y`
+-/
+
+-- S touto definicí limity posloupnosti budeme pracovat ve všech následujících cvičeních.
+def seq_limit (s : ℕ → ℝ) (a : ℝ) : Prop :=
+∀ ε : ℝ, ε > 0 → ∃ n₀ : ℕ, ∀ n : ℕ, n ≥ n₀ → |s n - a| ≤ ε
+
+
+-- Součet limit:
+-- Pokud [`u` converguje k `a`] a [`v` converguje k `b`],
+-- pak [`u + v` converguje k `a + b`].
+example (u v : ℕ → ℝ) (a b : ℝ) (hu : seq_limit u a) (hv : seq_limit v b) :
+  seq_limit (u + v) (a + b) :=
+begin
+  sorry,
+end
+
+-- Věta o dvou policajtech:
+-- Pokud [posloupnost `u` converguje k `z`] a stejně tak [posloupnost `v` converguje k `z`],
+-- pak [posloupnost `w` taková že `u ≤ w ≤ v` converguje k `z` taktéž].
+example (u v w : ℕ → ℝ) (z : ℝ) (hu : seq_limit u z) (hv : seq_limit v z)
+        (below : ∀ n, u n ≤ w n) (above : ∀ n, w n ≤ v n) :
+  seq_limit w z :=
+begin
+  sorry,
+end
+
+-- Každá posloupnost má nejvýše jednu limitu.
+example (w : ℕ → ℝ) (a b : ℝ) (ha : seq_limit w a) (hb : seq_limit w b) :
+  a = b :=
+begin
+  sorry,
+end
